1d53d0e73b4847af01f785e5a2d38e17a49e5b4c
Nouni EL Bachir <nouni.elbachir@gmail.com>
2019-09-22 17:54:26 +0100

    change UrlSupport visibility to package only instead of public

--- src/main/java/com/vectorly/api/rest/UrlSupport.java
+++ src/main/java/com/vectorly/api/rest/UrlSupport.java
@@ -7,7 +7,7 @@ import java.net.URL;
  * @author NOUNI El Bachir
  *
  */
-public final class UrlSupport {
+final class UrlSupport {
 	/**
 	 * @param url url to be converted
 	 * @return string representation of the URL without the trailing slashs
